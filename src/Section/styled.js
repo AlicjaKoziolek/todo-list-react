3e2d4f83ffd836bab3557b73da2dc0c27978d38c
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-09-06 11:30:20 +0200

    added @media queries to StyledHeader in Section component

--- src/Section/styled.js
+++ src/Section/styled.js
@@ -13,10 +13,14 @@ export const StyledSection = styled.section`
 export const StyledHeader = styled.header`
   display: grid;
   grid-template-columns: auto auto;
-  grid-gap: 20px;
+ 
   justify-content: space-between;
   align-items: center;
   border-bottom: 1px solid #dddada;
+
+  @media (max-width: 767px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 export const StyledTitle = styled.h2`
