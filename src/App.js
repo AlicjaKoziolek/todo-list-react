052727df76c0f9b2a71f43b152e17ba6219655f3
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-11-16 12:24:52 +0100

    created navigation for the website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Switch, Route, Link, Redirect } from "react-router-dom";
+import Author from "./features/author/Author";
 
-const App = () => <Tasks/>;
+const App = () => (
+  <HashRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">Zadania</Link>
+        </li>
+        <li>
+          <Link to="/autor">O autorze</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/zadania">
+          <Tasks />
+        </Route>
+        <Route path="/autor">
+          <Author />
+        </Route>
+        <Redirect to="/zadania" />
+      </Switch>
+    </nav>
+  </HashRouter>
+);
 
 export default App;
---
+++ src/features/author/Author.js
@@ -0,0 +1,34 @@
+import Container from "../../common/Container";
+import Header from "../../common/Header";
+import Section from "../../common/Section";
+
+export default () => (
+  <Container>
+    <Header title="O autorze strony" />
+    <Section
+      title="Alicja Koziołek"
+      body={
+        <>
+          <p>
+            Od ponad 10 lat jestem <strong>szczęśliwą żoną i mamą.</strong>
+            Uwielbiam spędzać czas w gronie rodziny i znajomych 👨‍👩‍👧‍👦.
+          </p>
+          <p>
+            W długie jesienno- zimowe wieczory często gramy w gry planszowe 🎲.
+            Posiadam również <strong>zamiłowania kulinarne</strong>- w
+            szczególności uwielbiam robić torty okolicznościowe 🎂.
+            <strong> Moją pasją jest również sport</strong>- w szczególności
+            fitness, rower i łyżworolki 🧘‍♀️🚵‍♀️.
+          </p>
+          <p>
+            Mimo, że ukończyłam studia na kierunku
+            <em>Zarządzanie logistyczno-marketingowe</em>, nie czuję aby była to
+            moja ścieżka, którą chciałabym kontynuować. Chciałabym znaleźć
+            pracę, która <strong>stanie się moją pasją</strong> i pozwoli mi
+            pogodzić obowiązki mamy i żony z pracą zawodową 🤩.
+          </p>
+        </>
+      }
+    />
+  </Container>
+);
