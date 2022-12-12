141ccb028f83324d39b51988b0377bce39c606da
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-12-12 12:25:52 +0100

    added button to download sample tasks

--- src/features/tasks/TasksPage/index.js
+++ src/features/tasks/TasksPage/index.js
@@ -5,18 +5,21 @@ import Header from "../../../common/Header";
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Search from "./Search";
+import FetchExampleTasksButton from "./FetchExampleTasksButton";
 
 function TasksPage() {
   return (
     <Container>
       <Header 
-        title="Lista zadań" />
-      <Section 
-        title="Dodaj nowe zadanie" 
-        body={<Form />} />
-      <Section 
-        title="Wyszukiwarka" 
-        body={<Search />} />
+      title="Lista zadań" />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form />}
+        extraHeaderContent={<FetchExampleTasksButton />}
+      />
+      <Section
+       title="Wyszukiwarka"
+       body={<Search />} />
       <Section
         title="Lista zadań"
         body={<TaskList />}
@@ -24,6 +27,6 @@ function TasksPage() {
       />
     </Container>
   );
-};
+}
 
 export default TasksPage;
