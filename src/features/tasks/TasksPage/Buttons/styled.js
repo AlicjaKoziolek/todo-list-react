041a26b24a7e2bcce62bce5d23dc2066b4813a3d
Alicja Koziolek <koziolek.alicja@gmail.com>
2022-12-12 12:34:25 +0100

    styling changed for StyledButtons

--- src/features/tasks/TasksPage/Buttons/styled.js
+++ src/features/tasks/TasksPage/Buttons/styled.js
@@ -1,29 +1,8 @@
 import styled from "styled-components";
 
-export const StyledButton = styled.button`
-  background-color: transparent;
-  color: ${({ theme }) => theme.color.teal};
-  border: none;
-  margin: 0 0 0 20px;
-  padding: 10px;
-  transition: color 0.3s;
-  cursor: pointer;
-
-  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-    flex-basis: 100%;
-  }
-
-  &:hover {
-    filter: brightness(110%);
-  }
-
-  &:active {
-    filter: brightness(120%);
-  }
-
-  &:disabled {
-    color: ${({ theme }) => theme.color.alto};
-  }
+export const StyledButtons = styled.div`
+  display: flex;
+  flex-wrap: wrap;
 `;
 
 export const StyledSection = styled.div`
